93f250c12212bd61546f8801c749b4353e712590
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-04-12 10:19:40 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import RealTimeClimateConditions from './RealTimeClimateConditions';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RealTimeClimateConditions />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
